f769f7ca55369b918dd8b19fc6f55a96b4b61a15
rftyghghj <gs1peacectr@hotmail.com>
2024-05-10 10:23:06 +0700

    update sdk commit

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-function uniquePaths(m, n) {
-  const dp = new Array(m).fill(0).map(() => new Array(n).fill(0));
-  for (let i = 0; i < m; i++) {
-    dp[i][0] = 1;
-  }
-  for (let j = 0; j < n; j++) {
-    dp[0][j] = 1;
-  }
-  for (let i = 1; i < m; i++) {
-    for (let j = 1; j < n; j++) {
-      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+function isValidParentheses(s) {
+  const stack = [];
+  const map = { "(": ")", "[": "]", "{": "}" };
+  for (const char of s) {
+    if (char in map) stack.push(char);
+    else {
+      const top = stack.pop();
+      if (map[top] !== char) return false;
     }
   }
-  return dp[m - 1][n - 1];
+  return stack.length === 0;
 }
