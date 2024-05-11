892c88e437fc5de80ad680b25949eabcb9ab0208
rftyghghj <gs1peacectr@hotmail.com>
2024-05-12 04:36:41 +0700

    demo task commit

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-function isValidParentheses(s) {
-  const stack = [];
-  const map = { "(": ")", "[": "]", "{": "}" };
-  for (const char of s) {
-    if (char in map) stack.push(char);
-    else {
-      const top = stack.pop();
-      if (map[top] !== char) return false;
-    }
-  }
-  return stack.length === 0;
+function averageArray(arr) {
+  return arr.reduce((acc, curr) => acc + curr, 0) / arr.length;
 }
