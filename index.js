7ddf34d70e870a172cb847cbed2a40b9d00800f5
rftyghghj <gs1peacectr@hotmail.com>
2024-05-13 15:04:37 +0700

    update aptos project

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-function kthSmallest(matrix, k) {
-  const flatten = matrix.reduce((acc, row) => acc.concat(row), []);
-  flatten.sort((a, b) => a - b);
-  return flatten[k - 1];
+function numSquares(n) {
+  const dp = new Array(n + 1).fill(Infinity);
+  dp[0] = 0;
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j * j <= i; j++) {
+      dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+    }
+  }
+  return dp[n];
 }
