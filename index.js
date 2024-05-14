8d5f6b2d3cc45af3ced8d63e09240dc4a762ff8c
rftyghghj <gs1peacectr@hotmail.com>
2024-05-14 13:06:20 +0700

    add more features

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-function numSquares(n) {
-  const dp = new Array(n + 1).fill(Infinity);
-  dp[0] = 0;
-  for (let i = 1; i <= n; i++) {
-    for (let j = 1; j * j <= i; j++) {
-      dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
-    }
-  }
-  return dp[n];
-}
+const initializeArrayWithRange = (end, start = 0) =>
+  Array.from({ length: end - start }, (_, i) => i + start);
